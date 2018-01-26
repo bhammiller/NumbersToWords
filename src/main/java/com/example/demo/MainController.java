e44ef0dae5339b67cf37dfb30b2d8d7eaa455e20
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-26 14:37:06 -0500

    Used some code I found online.

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,4 +1,16 @@
 package com.example.demo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class MainController {
+    @RequestMapping ("/")
+    public String magic_numberer(@RequestParam("number") int input_number){
+        NumbersToWords magicNumber = new NumbersToWords();
+        String result = magicNumber.convert(input_number);
+        return "User Input: " + input_number + "<br/>" + "Output: " + result;
+
+    }
 }
